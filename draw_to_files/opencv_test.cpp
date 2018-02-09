1820cb8644bdea276de2530d68714d51a11b29a9
Xing <xingmarc@yeah.net>
2018-02-08 23:56:34 -0800

    Add a simple but dumb implementation of atmoshere.

--- draw_to_files/opencv_test.cpp
+++ draw_to_files/opencv_test.cpp
@@ -4,16 +4,21 @@
 
 #include <iostream>
 #include <string>
-
-#define w 400
+#include <random>
+#include <utility>
+#include <vector>
 
 using namespace cv;
 
 using namespace std;
 
+const int w = 1000;
 
-void random_paint() {
-}
+int all = 1000000;
+int N2 =   780840;
+int O2 =   209460;
+int Ar =     9340;
+int Other =   360;
 
 int main( int argc, char** argv )
 {
@@ -34,21 +39,77 @@ int main( int argc, char** argv )
     imshow( "Display window", image );                // Show our image inside it.
 
 
-    Mat atom_image = Mat::zeros( w, w, CV_8UC3 );
+    Mat atom_image = Mat::ones( w, w, CV_8UC3 );
     const int channels = atom_image.channels();
     cout << channels << endl;
-    MatIterator_<Vec3b> it;
+
+
+    vector<pair<int, int>> vp;
+    vp.push_back(make_pair(1, N2));
+    vp.push_back(make_pair(N2 + 1, N2 + O2));
+    vp.push_back(make_pair(N2 + O2 + 1, N2 + O2 + Ar));
+    vp.push_back(make_pair(N2 + O2 + Ar + 1, all));
+
+    default_random_engine e;
+
+    MatIterator_<Vec3b> it, end;
     it = atom_image.begin<Vec3b>();
+    end = atom_image.end<Vec3b>();
 
-    it += 800;
+    for (;it != end; it++)
+    {
+    	uniform_int_distribution<int> distribution(1, all);
+	int r = distribution(e);
+	cout << r << endl;
 
-    (*it)[0] = 255;
-    (*it)[1] = 255;
-    (*it)[2] = 255;
+	if (r >= vp[3].first && r <= vp[3].second)
+	{
+		(*it)[0] = 255;
+		(*it)[1] = 1;
+		(*it)[2] = 1;
+		all -= 1;
+		vp[3].second = all;
+	}
+	else if (r >= vp[2].first && r <= vp[2].second)
+	{
+		(*it)[0] = 1;
+		(*it)[1] = 255;
+		(*it)[2] = 1;
+		all -= 1;
+		vp[3].second = all;
+		vp[3].first -= 1;
+		vp[2].second -= 1;
+	}
+	else if (r >= vp[1].first && r <= vp[1].second)
+	{
+		(*it)[0] = 1;
+		(*it)[1] = 1;
+		(*it)[2] = 255;
 
-    it += 1600;
-    
-    (*it)[0] = 255;
+		all -= 1;
+		vp[3].second = all;
+		vp[3].first -= 1;
+		vp[2].second -= 1;
+		vp[2].first -= 1;
+		vp[1].second -= 1;
+	}
+
+	else // if (r >= vp[0].first && r <= vp[0].second)
+	{
+		(*it)[0] = 255;
+		(*it)[1] = 255;
+		(*it)[2] = 255;
+
+		all -= 1;
+		vp[3].second = all;
+		vp[3].first -= 1;
+		vp[2].second -= 1;
+		vp[2].first -= 1;
+		vp[1].second -= 1;
+		vp[1].first -= 1;
+	}
+    }
+
     imshow("test window", atom_image);
     waitKey(0);
     return 0;
