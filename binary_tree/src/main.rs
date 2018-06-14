36466ea625fc8db39889fe5c69a526eff36fb5a9
Xing <xingmarc@yeah.net>
2018-06-13 23:05:35 -0700

    Another not very successful try.

--- binary_tree/src/main.rs
+++ binary_tree/src/main.rs
@@ -10,7 +10,60 @@ enum Type {
 
 struct NodeTag {
     number: f32,
-    children: [Box<NodeTag>; 2]
+    left: Option<Box<NodeTag>>,
+    right: Option<Box<NodeTag>>,
+    t: Type
+}
+
+impl NodeTag {
+    fn new(number: f32) -> NodeTag {
+        NodeTag {
+            number: number,
+            left: None,
+            right: None,
+            t: Type::NUM
+        }
+    }
+
+    fn new_type(t: Type) -> NodeTag {
+        NodeTag {
+            number: 0.0,
+            left: None,
+            right: None,
+            t: t
+        }
+    }
+}
+
+fn parse(s: &String) -> NodeTag {
+    let mut chars = s.chars();
+    while let Some(c) = chars.next() {
+        if c == ' ' {
+            continue;
+        }
+
+        if c.is_digit(10) {
+            println!("is digit! {}", c);
+            if let Some(d) = c.to_digit(10) {
+                return NodeTag::new(d as f32);
+            } else {
+                panic!("Failed in char to digit!");
+            }
+        } else {
+            let mut n = NodeTag::new_type(Type::ADD);
+            match c {
+                '+' => n.t = Type::ADD,
+                '-' => n.t = Type::SUB,
+                '*' => n.t = Type::MUL,
+                '/' => n.t = Type::DIV,
+                _ => n.t = Type::ADD
+            };
+
+            return n;
+        }
+    }
+
+    return NodeTag::new(1.0);
 }
 
 fn main() {
@@ -20,16 +73,7 @@ fn main() {
         return
     }
     println!("{}", args[1]);
-    let expression = &args[1];
+    // let expression = &args[1];
+    parse(&args[1]);
 
-    let mut chars = expression.chars();
-    while let Some(c) = chars.next() {
-        if c == ' ' {
-            println!("Whitespace");
-            continue;
-        }
-        println!("{}", c);
-    }
-
-    println!("Hello, world!");
 }
